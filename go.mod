c241118d8c1c08e1944c9990e88e498b4182bcb1
Mario Demuth <mario.demuth@voestalpine.com>
2019-12-02 13:59:01 +0100

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cognicraft/sqlutil
+
+go 1.12
---
+++ transact.go
@@ -0,0 +1,29 @@
+package sqlutil
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func Transact(db *sql.DB, txFunc func(*sql.Tx) error) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			switch p := p.(type) {
+			case error:
+				err = p
+			default:
+				err = fmt.Errorf("%s", p)
+			}
+		}
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+	return txFunc(tx)
+}
